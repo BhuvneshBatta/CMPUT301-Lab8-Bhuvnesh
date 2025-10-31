f373d9bad0a2ca781132165063ef6af33431ef31
bhuvneshbatta <bhuvneshbatta23@gmail.com>
2025-10-31 15:45:27 -0600

    Add failing test for deleteCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -14,4 +14,21 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist yet
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity_removesCity() {
+        CustomList list = new CustomList();
+        City edm = new City("Edmonton", "AB");
+        City cal = new City("Calgary", "AB");
+
+        list.addCity(edm);
+        list.addCity(cal);
+
+        list.deleteCity(cal);
+
+        assertFalse(list.hasCity(cal));  // should be gone
+        assertTrue(list.hasCity(edm));   // other city still there
+    }
+
+
+
 }
